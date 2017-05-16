bc6b32d163e5a40a02d7d838060cd30cb1bcacda
dylan riley <dylan.riley@nice.org.uk>
2017-05-16 23:07:03 +0100

    Test

--- src/BlockBreaker/Main.java
+++ src/BlockBreaker/Main.java
@@ -1,8 +1,16 @@
 package BlockBreaker;
 
+import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 
 public class Main {
@@ -10,28 +18,36 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame obj = new JFrame();
+		JFrame frame;
 		
-		boolean MenuOpen = true;
-		if(MenuOpen){
-			Menu menu = new Menu();
-			obj.add(menu);
+		String gamerTag = "";
+		
+		frame = new JFrame("BlockBreaker");
+		
+		Menu menu = new Menu();
+		GamePlay gamePlay = new GamePlay();
 			
-		}
-		else{
-			GamePlay gamePlay = new GamePlay();
-			obj.add(gamePlay);
-		}
+		CardLayout cl = new CardLayout();
 		
-		obj.setBounds(10,10,700,600);
-		obj.setTitle("BlockBreaker");
-		obj.setResizable(false);
-		obj.setVisible(true);
-		obj.setDefaultCloseOperation(obj.EXIT_ON_CLOSE);
-		
-		
-		
+			JPanel containerPanel = new JPanel();
+			containerPanel.setLayout(cl);
+			containerPanel.add(menu, "1");
+			containerPanel.add(gamePlay, "2");
+			cl.show(containerPanel, "1");
+			
+			frame.setBounds(10,10,700,600);
+			frame.setResizable(false);
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+			frame.add(containerPanel);
+			while(menu.formSubmitted()){
+				gamerTag = menu.getGamerTag();
 
+			}
+			
+			cl.show(containerPanel, "2");
+			
+			
 	}
 
 }
--- src/BlockBreaker/Menu.java
+++ src/BlockBreaker/Menu.java
@@ -1,62 +1,98 @@
 package BlockBreaker;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.Timer;
 
-public class Menu extends JPanel implements KeyListener, ActionListener{
-	private JButton button;
-	private JTextField input;
-	
-		public Menu() {
-			setLayout(null);
-			button = new JButton("Start!");
-			input = new JTextField("",30);
-			button.setBounds(400, 170, 90, 30);
-			input.setBounds(300, 170, 90, 30);
-			add(input);
-			add(button);
-		}
+public class Menu extends JPanel {
+	String error = "error";
+	String gamerTag = "";
+	Font f = new Font("serif", Font.BOLD, 25);
+	JLabel errorMessage;
+	JTextField textbox;
+	JButton confirmButton;
+	boolean submit = false;
+
+	public Menu() {
+		Container contentPane = this;
+
+		SpringLayout layout = new SpringLayout();
+		contentPane.setLayout(layout);
 		
-		public void paint(Graphics g){
-			//Background
-			g.setColor(Color.black);
-			g.fillRect(1, 1, 692, 592);
-			
-			
-			g.setColor(Color.green);
-			g.setFont(new Font("serif", Font.BOLD, 35));
-			g.drawString("Welcome! Enter your GamerTag.", 90,150);
-			
-			g.dispose();
-		}
+		errorMessage = new JLabel(error);
+		errorMessage.setFont(f);
+		Component label = new JLabel("Enter GamerTag");
+		label.setFont(f);
+		textbox = new JTextField(20);
+		textbox.setFont(f);
+		confirmButton = new JButton("Confirm");
+		confirmButton.setPreferredSize(new Dimension(80, 36));
+		
+		
+		contentPane.add(errorMessage);
+		contentPane.add(label);
+		contentPane.add(textbox);
+		contentPane.add(confirmButton);
+		
+		
+		layout.putConstraint(SpringLayout.WEST, errorMessage, 150, SpringLayout.WEST,
+				contentPane);
+		layout.putConstraint(SpringLayout.NORTH, errorMessage, 250, SpringLayout.NORTH,
+				contentPane);
+		layout.putConstraint(SpringLayout.WEST, label, 150, SpringLayout.WEST,
+				contentPane);
+		layout.putConstraint(SpringLayout.NORTH, label, 150, SpringLayout.NORTH,
+				contentPane);
+		layout.putConstraint(SpringLayout.NORTH, textbox, 200,
+				SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, textbox, 150, SpringLayout.WEST,
+				contentPane);
+		layout.putConstraint(SpringLayout.WEST, confirmButton, 575, SpringLayout.WEST,
+				contentPane);
+		layout.putConstraint(SpringLayout.NORTH, confirmButton, 200, SpringLayout.NORTH,
+				contentPane);
 
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+		
+		
+		confirmButton.addActionListener(new ActionListener()
+		{
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  gamerTag = textbox.getText();
+				  if (gamerTag.length() < 8) {
+					  error = "Gamer tag must be atleaste 8 characters";
+						errorMessage.setText(error); 
+					}
+				  else
+					  submit = true;
+			  }
+			});
 
-		public void keyPressed(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+	}
+	
+	public boolean formSubmitted(){
+		return submit;
+	}
+	public String getGamerTag(){
+		return gamerTag;
+	}
 
-		public void keyReleased(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void keyTyped(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+	
 }
